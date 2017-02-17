992434b898cddfe9beb0a8fad093b818c76716f9
shijie <tmyk104117@outlook.com>
2017-02-17 12:53:56 +0800

    add gunicorn

--- imeili100/settings.py
+++ imeili100/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '80huzdno0d(%0)ucgqmz-p5etw^(gavgkcp(mup%j)ndu7jth^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['imeili100app','localhost','127.0.0.1']
 
 
 # Application definition
--- imeili100/urls.py
+++ imeili100/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-import settings
+from . import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('imeili100app.urls')),
